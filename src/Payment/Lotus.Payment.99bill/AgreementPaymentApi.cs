2265e11e2f7a503658ce550536fd75041e1a28ec
苏锡波 <vsxibo@163.com>
2018-09-11 09:47:28 +0800

    remove

---
+++ src/Payment/Lotus.Payment.99bill/AgreementPaymentApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lotus.Payment._99bill
+{
+    /// <summary>
+    /// 协议支付API
+    /// </summary>
+    public static class AgreementPaymentApi
+    {
+    }
+}
