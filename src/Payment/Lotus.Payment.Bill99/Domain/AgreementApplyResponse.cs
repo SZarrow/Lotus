57bc25039c53b41bb6b39f46c73a267f25cd2217
苏锡波 <vsxibo@163.com>
2018-09-17 10:40:42 +0800

    update for AgreementApply()

--- src/Payment/Lotus.Payment.Bill99/Domain/AgreementApplyResponse.cs
+++ src/Payment/Lotus.Payment.Bill99/Domain/AgreementApplyResponse.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Serialization;
+using Lotus.Serialization;
 
 namespace Lotus.Payment.Bill99.Domain
 {
     public class AgreementApplyResponse : MasMessage
     {
+        [XElement("merchantId")]
         public String MerchantId { get; set; }
+        [XElement("terminalId")]
         public String TerminalId { get; set; }
+        [XElement("customerId")]
         public String CustomerId { get; set; }
+        [XElement("externalRefNumber")]
         public String ExternalRefNumber { get; set; }
+        [XElement("storablePan")]
         public String StorablePan { get; set; }
+        [XElement("paytoken")]
         public String Paytoken { get; set; }
+        [XElement("token")]
         public String Token { get; set; }
+        [XElement("responseCode")]
         public String ResponseCode { get; set; }
+        [XElement("responseTextMessage")]
         public String ResponseTextMessage { get; set; }
     }
 }
--- test/Lotus.Payment.Bill99.Tests/AgreementPaymentApiTest.cs
+++ test/Lotus.Payment.Bill99.Tests/AgreementPaymentApiTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Xml.Serialization;
@@ -16,18 +18,26 @@ namespace Lotus.Payment.Bill99.Tests
         {
             using (var handler = new HttpClientHandler())
             {
-                handler.ClientCertificates.Add(new X509Certificate2("~/10411004511201290.pfx", "vpos123"));
+                using (var fs = File.OpenRead(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "10411004511201290.pfx")))
+                {
+                    Byte[] certData = new Byte[fs.Length];
+                    fs.Read(certData, 0, certData.Length);
+                    handler.ClientCertificates.Add(new X509Certificate2(certData, "vpos123"));
+                }
+
                 using (var client = new HttpClient(handler))
                 {
-                    String auth = "";
+                    ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+
+                    String auth = Convert.ToBase64String(Encoding.ASCII.GetBytes("104110045112012:vpos123")); 
                     client.DefaultRequestHeaders.Add("Authorization", $"Basic {auth}");
-                    var api = new AgreementPaymentApi(client, "812310060510214", "13196988");
+                    var api = new AgreementPaymentApi(client, "104110045112012", "00002012");
                     var result = api.AgreementApply("https://sandbox.99bill.com:9445/cnp/ind_auth", new AgreementApplyRequest()
                     {
                         BindType = "0",
                         CustomerId = "C0001",
                         ExternalRefNumber = "ERF0001",
-                        Pan = "Pan0001",
+                        Pan = "6217002000038983690",
                         PhoneNO = "13382185203",
                         Version = "1.0"
                     });
@@ -35,6 +45,12 @@ namespace Lotus.Payment.Bill99.Tests
                     Assert.NotNull(result.Value);
                 }
             }
+        }
+
+        //证书建立安全通道
+        public Boolean CheckValidationResult(Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true;
         }
     }
 }
