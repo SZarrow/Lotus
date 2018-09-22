22a59fb350544d95beb924ccfdae8b82897cefcd
苏锡波 <vsxibo@163.com>
2018-09-22 21:19:36 +0800

    remove discard project.

---
+++ src/Maintain/Lotus.Scheduler.Web/Controllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lotus.Scheduler.Web.Controllers
+{
+    public class JobController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
