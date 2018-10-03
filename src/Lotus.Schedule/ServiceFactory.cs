de1fdd7111f1c5b4984483bfb896aecb4233f1ce
苏锡波 <vsxibo@163.com>
2018-10-03 21:07:33 +0800

    add Lotus.Schedule.

---
+++ src/Lotus.Schedule/ServiceFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Lotus.Schedule
+{
+    /// <summary>
+    /// 服务工厂类，用来创建Windows服务。
+    /// </summary>
+    public static class ServiceFactory
+    {
+        /// <summary>
+        /// 运行服务，如果服务不存在则创建一个。
+        /// </summary>
+        /// <param name="service">服务描述</param>
+        /// <param name="jobsConfigFilePath">任务配置文件，具体参见bin目录下的QuartzJobs.config文件。</param>
+        public static void RunService(ServiceDescription service, String jobsConfigFilePath)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if (String.IsNullOrWhiteSpace(jobsConfigFilePath))
+            {
+                throw new ArgumentNullException(nameof(jobsConfigFilePath));
+            }
+
+            if (!File.Exists(jobsConfigFilePath))
+            {
+                throw new FileNotFoundException(jobsConfigFilePath);
+            }
+
+            HostFactory.Run(x =>
+            {
+                x.Service(() =>
+                {
+                    return new QuartzService(jobsConfigFilePath);
+                });
+
+                x.SetDescription(service.Description);
+                x.SetDisplayName(service.DisplayName);
+                x.SetServiceName(service.ServiceName);
+                x.EnablePauseAndContinue();
+            });
+        }
+    }
+}
---
+++ test/Lotus.Schedule.Tests/LocalJobForCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotus.Schedule.Tests
+{
+    public class LocalJobForCreate : LotusJob
+    {
+        protected override Task Execute()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"{DateTime.Now.ToString("MM-dd HH:mm:ss:ffff")} -> {this.GetType().Name} Executed.");
+            });
+        }
+    }
+}
