e80808bb705dfa383d3ae0f2953379765961e70a
苏锡波 <vsxibo@163.com>
2018-10-03 22:58:34 +0800

    remove reference of DotNetWheels.Core.

---
+++ src/Lotus.Core/ObjectResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotus.Core
+{
+    [Serializable]
+    public class ObjectResult : XResult<Object>
+    {
+        public ObjectResult(Object value, params Exception[] exceptions) : base(value, exceptions) { }
+    }
+}
